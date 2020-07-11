dc7b8124a0b0d4de3c32a576db88745e379824d6
suguru_katsuyama <zgok105@yahoo.co.jp>
2020-07-11 11:12:32 +0900

    初期画面を表示するため

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root "posts#index"
+  resources :posts
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
